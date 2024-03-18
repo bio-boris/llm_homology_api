386d8ec92486fb5e9e00359765b93fa6880dd84b
Boris <bio-boris@github.com>
2024-03-18 11:52:21 -0500

    ss

--- llm_homology_api/src/routes/similarity.py
+++ llm_homology_api/src/routes/similarity.py
@@ -58,23 +58,41 @@ async def calculate_similarity(request: Request, sr: SimilarityRequest):
 
     search_results, query_embeddings = ss.search(query_sequences, top_k=sr.max_hits)
 
+    # BatchedSearchResults(total_scores=[
+    #     [0.9999261498451233, 0.9999261498451233, 0.9999261498451233, 0.9999261498451233, 0.9999165534973145, 0.9999165534973145, 0.9999165534973145,
+    #      0.9998984336853027, 0.9998682737350464, 0.9998661875724792],
+    #     [0.9932831525802612, 0.9930819869041443, 0.9930461049079895, 0.9929633140563965, 0.9927730560302734, 0.992717981338501, 0.9927024245262146,
+    #      0.9921532273292542, 0.9919975996017456, 0.9907838702201843]],
+    #                      total_indices=[[387541, 126527, 126524, 126526, 126522, 126520, 126521, 387535, 387536, 126531],
+    #                                     [301217, 301220, 127059, 281436, 214578, 214579, 127042, 127046, 214572, 281423]])
+
+    pruned_hits = []
+    total_hits = []
+    for result in search_results:
+        pruned_result = []
+        for score, ind in zip(search_results.total_scores, search_results.total_indices):
+            total_hits.append(len(ind))
+            if score > threshold:
+                # Get the sequence tags found by the search
+                seq_id = ss.get_sequence_tags(ind)
+
+                embedding = []
+                if not discard_embeddings:
+                    embedding = ss.get_sequence_embeddings(ind)
+                    # Convert to python list for REST API
+                    embedding = [float(i) for i in embedding[0].tolist()]
+                pruned_result.append(HitDetail(HitID=seq_id, Score=score, Embedding=embedding))
+
+        pruned_hits.append(pruned_result)
+
     proteins = []
     for i, protein in enumerate(query_sequences):
-        putative_hits = search_results[i]
 
-        pprint(putative_hits)
-        pruned_hits = []
-        for score, ind in zip(putative_hits.total_scores, putative_hits.total_indices):
-            if score > threshold:
-                seq_id = ss.get_sequence_tags(ind)
-                embedding = ss.get_sequence_embeddings(ind)
-                embedding_list = [float(i) for i in embedding[0].tolist()]
-                pruned_hits.append(HitDetail(HitID=seq_id, Score=score, Embedding=embedding_list))
         qp = QueryProtein(
             QueryId=protein.id,
             Embedding=query_embeddings[i] if not discard_embeddings else [],
-            total_hits=len(putative_hits),
-            Hits=pruned_hits,
+            total_hits=total_hits[i],
+            Hits=pruned_hits[i],
         )
         proteins.append(qp)
 
